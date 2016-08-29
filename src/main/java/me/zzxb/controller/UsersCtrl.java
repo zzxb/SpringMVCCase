e0521565c338aee27a43007a12d96424badb3db0
zzxb <cnhidba@126.com>
2016-08-29 14:08:48 +0800

    新增跳转到视图类型

--- src/main/java/me/zzxb/controller/UsersCtrl.java
+++ src/main/java/me/zzxb/controller/UsersCtrl.java
@@ -3,16 +3,16 @@ package me.zzxb.controller;
 import me.zzxb.pojo.Txls;
 import me.zzxb.pojo.Users;
 import me.zzxb.service.UsersService;
-
-import java.util.List;
-import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by zzxb on 16/8/24.
@@ -46,5 +46,12 @@ public class UsersCtrl {
     public List<Txls> listTxlsByUserName(String uname){
         List<Txls> data = usersService.getTxlsByUserName(uname);
         return data;
+    }
+
+    @RequestMapping(value = "/vlist")
+    public String listTxlsByUsersNameView(String uname, ModelMap modelMap){
+        List<Txls> data = usersService.getTxlsByUserName(uname);
+        modelMap.addAttribute("lxrlist",data);
+        return "vlist";
     }
 }
