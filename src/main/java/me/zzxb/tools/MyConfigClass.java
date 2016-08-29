0fd03a14b1fc304f42c951955485fbaf63e01b1b
zzxb <cnhidba@126.com>
2016-08-29 14:18:01 +0800

    新增了一些注释

--- src/main/java/me/zzxb/tools/MyConfigClass.java
+++ src/main/java/me/zzxb/tools/MyConfigClass.java
@@ -3,6 +3,9 @@ package me.zzxb.tools;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
 
+/**
+ * 解决could not initialize proxy - no Session错误的解决方案
+ */
 public class MyConfigClass extends ObjectMapper {
 
     public MyConfigClass() {
