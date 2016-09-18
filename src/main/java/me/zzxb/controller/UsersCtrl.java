00aa032287f98a379fb8857098141c0ec80caa18
zzxb <cnhidba@126.com>
2016-09-18 17:27:02 +0800

    新增了基于标准restful的请求处理，包括:get,post,delete,put等

--- src/main/java/me/zzxb/controller/UsersCtrl.java
+++ src/main/java/me/zzxb/controller/UsersCtrl.java
@@ -6,6 +6,7 @@ import me.zzxb.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -27,28 +28,53 @@ public class UsersCtrl {
 
     @RequestMapping(value = "/",method = RequestMethod.GET)
     public String index(){
-        return "index";
+        return "redirect:/index.jsp";
     }
 
-    @RequestMapping(value = "/register")
+    @RequestMapping(value = "/register/{userName}/{upwd}",method = RequestMethod.POST)
     @ResponseBody
-    public Map<String,String> register(String userName,String upwd){
+    public Map<String,String> register(@PathVariable String userName,@PathVariable String upwd){
         Users myUsers = new Users(userName,upwd);
         usersService.addUsers(myUsers);
         Map<String,String> data = new HashMap<String,String>();
-        data.put("state","1");
+        data.put("state","200001");
         data.put("info","插入成功!");
         return data;
     }
 
-    @RequestMapping(value = "/list")
+    @RequestMapping(value = "/update/{uname}/{upwd}/{newUpwd}",method = RequestMethod.PUT)
+    @ResponseBody
+    public Map<String,Object> updateUserPwd(@PathVariable String uname, @PathVariable String upwd,@PathVariable String newUpwd){
+        boolean isSucc = usersService.updateUsers(uname,upwd,newUpwd);
+        Map<String,Object> data = new HashMap<>();
+        if(isSucc) {
+            data.put("state","200001");
+            data.put("info", "更新成功!");
+        }else{
+            data.put("state","500001");
+            data.put("info","更新失败！");
+        }
+        return data;
+    }
+
+    @RequestMapping(value = "/del/{uid}",method = RequestMethod.DELETE)
+    @ResponseBody
+    public Map<String,Object> delUsers(@PathVariable String uid){
+        usersService.delUsers(Integer.parseInt(uid));
+        Map<String,Object> data = new HashMap<>();
+        data.put("state","200001");
+        data.put("info","删除成功!");
+        return data;
+    }
+
+    @RequestMapping(value = "/list",method = RequestMethod.GET  )
     @ResponseBody
     public List<Txls> listTxlsByUserName(String uname){
         List<Txls> data = usersService.getTxlsByUserName(uname);
         return data;
     }
 
-    @RequestMapping(value = "/vlist")
+    @RequestMapping(value = "/vlist",method = RequestMethod.GET )
     public String listTxlsByUsersNameView(String uname, ModelMap modelMap){
         List<Txls> data = usersService.getTxlsByUserName(uname);
         modelMap.addAttribute("lxrlist",data);
